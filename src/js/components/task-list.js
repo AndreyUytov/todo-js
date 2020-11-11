220f0a5425907a5e5f80dead7651f61bf0ace8d9
Андрей <uytov1991@gmail.com>
2020-11-11 19:54:08 +0300

    небольшой рефакторинг)

--- src/js/components/task-list.js
+++ src/js/components/task-list.js
@@ -3,17 +3,15 @@ import TaskLi from './task-elem'
 class TaskUl extends HTMLElement {
   constructor(tasks) {
     super()
-    this.tasks = tasks
-  }
-  connectedCallback() {
     this.attachShadow({ mode: 'open' })
-    const elements = this.tasks.map((el) => {
+    this.elements = tasks.map((el) => {
       return new TaskLi(el)
     })
-    const ul = document.createElement('ul')
-    ul.className = 'list'
-    ul.append(...elements)
-    this.shadowRoot.append(ul)
+    this.ul = document.createElement('ul')
+    this.ul.append(...this.elements)
+  }
+  connectedCallback() {
+    this.shadowRoot.append(this.ul)
   }
 }
 
