4b07b00f7ad84b1b75147b6ab4b39c574cd3164e
Андрей <uytov1991@gmail.com>
2020-11-17 19:12:01 +0300

    добавил хедер мок и стили

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import './styles/index.scss'
 
+import createStore from './js/model/create-store'
+import reducer from './js/model'
+
+import Header from './js/components/header'
 import TaskUl from './js/components/tasks-list'
 
+const header = new Header()
 const taskList = new TaskUl([
   { id: 1, value: 'разработать интерфейс программы' },
   { id: 2, value: 'two' },
   { id: 3, value: 'tree', isDone: true },
 ])
 
-document.body.append(taskList)
+root.append(header, taskList)
 
 setTimeout(() => {
   console.log(taskList.getTasks())
